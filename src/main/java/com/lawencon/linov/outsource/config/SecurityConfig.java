dcf22e3d02e4ed1508fa1db45432dd32be76828d
Irsyad Jamal Pratama Putra <irsyadjpratamap@gmail.com>
2019-09-08 23:35:15 +0700

    Create custom user details

---
+++ src/main/java/com/lawencon/linov/outsource/config/SecurityConfig.java
@@ -0,0 +1,6 @@
+package com.lawencon.linov.outsource.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
---
+++ src/main/java/com/lawencon/linov/outsource/security/CustomUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.lawencon.linov.outsource.security;
+
+import com.lawencon.linov.outsource.model.authentication.User;
+import com.lawencon.linov.outsource.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.nio.file.attribute.UserPrincipal;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with username or email: " + usernameOrEmail));
+        return UserPrincipal.create(user);
+    }
+
+    @Transactional
+    public UserDetails loadUserById(Long id){
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new ResorceNotFoundException("User", "id", id)
+        )
+        return UserPrincipa.create();
+    }
+}
