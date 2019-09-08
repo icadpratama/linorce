61e85b97c4df4f3cf17b73cc7ace9697c366c3ac
Irsyad Jamal Pratama Putra <irsyadjpratamap@gmail.com>
2019-09-08 23:49:26 +0700

    Create JwtAuthenticationEntryPoint

--- src/main/java/com/lawencon/linov/outsource/config/SecurityConfig.java
+++ src/main/java/com/lawencon/linov/outsource/config/SecurityConfig.java
@@ -1,6 +1,13 @@
 package com.lawencon.linov.outsource.config;
 
+import com.lawencon.linov.outsource.security.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    CustomUserDetailsService customUserDetailsService;
+
+
 }
