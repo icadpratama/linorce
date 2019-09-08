f46065ab635c211661ad131aed08f790b1db4629
Irsyad Jamal Pratama Putra <irsyadjpratamap@gmail.com>
2019-09-09 00:07:33 +0700

    Add JwtAuthentication and Ousource response

--- src/main/java/com/lawencon/linov/outsource/payload/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.lawencon.linov.outsource.payload;
---
+++ src/main/java/com/lawencon/linov/outsource/payload/response/OutsourceResponse.java
@@ -0,0 +1,27 @@
+package com.lawencon.linov.outsource.payload.response;
+
+public class OutsourceResponse {
+    private Boolean success;
+    private String message;
+
+    public OutsourceResponse(Boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
