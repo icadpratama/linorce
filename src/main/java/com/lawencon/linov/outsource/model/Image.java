177cb2ec935d84ea7b174d6c45a1f76d284e2b25
Irsyad Jamal Pratama Putra <irsyadjpratamap@gmail.com>
2019-09-14 20:41:02 +0700

    Add Image model

---
+++ src/main/java/com/lawencon/linov/outsource/model/Image.java
@@ -0,0 +1,66 @@
+package com.lawencon.linov.outsource.model;
+
+import com.lawencon.linov.outsource.model.audit.UserDateAudit;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tables")
+public class Image extends UserDateAudit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "object_name")
+    private String objectName;
+
+    @Column(name = "bucket_name")
+    private String bucketName;
+
+    @Column(name = "size")
+    private Long size;
+
+    @Column(name = "content_type")
+    private String contentType;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getObjectName() {
+        return objectName;
+    }
+
+    public void setObjectName(String objectName) {
+        this.objectName = objectName;
+    }
+
+    public String getBucketName() {
+        return bucketName;
+    }
+
+    public void setBucketName(String bucketName) {
+        this.bucketName = bucketName;
+    }
+
+    public Long getSize() {
+        return size;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+}
