c142cdcdbccb884e3acc94e8af90c007b60c4847
Irsyad Jamal Pratama Putra <irsyadjpratamap@gmail.com>
2019-09-14 20:42:46 +0700

    Add Image repository

---
+++ src/main/java/com/lawencon/linov/outsource/repository/ImageRepository.java
@@ -0,0 +1,7 @@
+package com.lawencon.linov.outsource.repository;
+
+import com.lawencon.linov.outsource.model.Image;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ImageRepository extends CrudRepository<Image, Long> {
+}
