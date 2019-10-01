0c0f41f0c7b02a14bbff2452b469e1eb04b9ef1c
Irsyad Jamal Pratama Putra <irsyadjpratamap@gmail.com>
2019-10-02 02:15:22 +0700

    Add transaction_logs table

--- src/main/resources/db.migration/V1__schema.sql
+++ src/main/resources/db.migration/V1__schema.sql
@@ -28,6 +28,22 @@ CREATE TABLE user_roles (
     CONSTRAINT fk_user_roles_user_id FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
+CREATE TABLE images(
+    id SERIAL8 NOT NULL,
+    object_name VARCHAR(50),
+    bucket_name VARCHAR(50),
+    size INT8,
+    content_type VARCHAR(50),
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    created_by INT8 DEFAULT NULL,
+    updated_by INT8 DEFAULT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE images
+ADD CONSTRAINT image_uq UNIQUE (object_name, bucket_name);
+
 CREATE TABLE item_requests(
     id SERIAL8 NOT NULL,
     name VARCHAR(50),
@@ -184,22 +200,6 @@ ALTER TABLE over_times
 -- INSERT INTO roles(id, name) values (2, 'ROLE_USER');
 -- INSERT INTO roles(id, name) values (3, 'ROLE_HR');
 
-CREATE TABLE images(
-    id SERIAL8 NOT NULL,
-    object_name VARCHAR(50),
-    bucket_name VARCHAR(50),
-    size INT8,
-    content_type VARCHAR(50),
-    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamp DEFAULT CURRENT_TIMESTAMP,
-    created_by INT8 DEFAULT NULL,
-    updated_by INT8 DEFAULT NULL,
-    PRIMARY KEY (id)
-);
-
-ALTER TABLE images
-ADD CONSTRAINT image_uq UNIQUE (object_name, bucket_name);
-
 CREATE TABLE absences(
     id SERIAL8 NOT NULL,
     location VARCHAR(255),
@@ -216,3 +216,14 @@ CREATE TABLE absences(
     updated_by INT8 DEFAULT NULL,
     PRIMARY KEY (id)
 );
+
+-- User transaction logs
+CREATE TABLE transaction_logs(
+    transaction_type VARCHAR(250),
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    created_by INT8 DEFAULT NULL,
+    updated_by INT8 DEFAULT NULL
+);
+
+-- User Activity Logs, login, logout, profile manipulation,
