5aab520de8573fd288a735006f7b2738f97306a9
Irsyad Jamal Pratama Putra <irsyadjpratamap@gmail.com>
2019-10-02 00:03:21 +0700

    Add Financial and Over Time model

---
+++ src/main/java/com/lawencon/linov/outsource/model/claim/FinancialClaim.java
@@ -0,0 +1,9 @@
+package com.lawencon.linov.outsource.model.claim;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "financial_claims")
+public class FinancialClaim {
+}
---
+++ src/main/java/com/lawencon/linov/outsource/model/claim/OverTime.java
@@ -0,0 +1,9 @@
+package com.lawencon.linov.outsource.model.claim;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "over_times")
+public class OverTime {
+}
