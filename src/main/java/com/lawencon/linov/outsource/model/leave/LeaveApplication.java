73b90c31873462b174ca7741bd1e01fdf4ca6d82
Irsyad Jamal Pratama Putra <irsyadjpratamap@gmail.com>
2019-10-02 00:01:50 +0700

    Add Leave Application model

---
+++ src/main/java/com/lawencon/linov/outsource/model/leave/LeaveApplication.java
@@ -0,0 +1,9 @@
+package com.lawencon.linov.outsource.model.leave;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "leave_applications")
+public class LeaveApplication {
+}
