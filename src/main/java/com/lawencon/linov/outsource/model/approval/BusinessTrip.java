c897893b092827ba1383761fde352116ee8f59ba
Irsyad Jamal Pratama Putra <irsyadjpratamap@gmail.com>
2019-10-02 00:00:33 +0700

    Add Business Trip, General Approval, Support From Home model

---
+++ src/main/java/com/lawencon/linov/outsource/model/approval/BusinessTrip.java
@@ -0,0 +1,9 @@
+package com.lawencon.linov.outsource.model.approval;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "business_trips")
+public class BusinessTrip {
+}
---
+++ src/main/java/com/lawencon/linov/outsource/model/approval/SupportFromHome.java
@@ -0,0 +1,9 @@
+package com.lawencon.linov.outsource.model.approval;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "support_homes")
+public class SupportFromHome {
+}
