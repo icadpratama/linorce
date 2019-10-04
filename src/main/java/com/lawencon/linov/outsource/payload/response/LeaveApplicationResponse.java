08f47856a6d113252b0a54fa8d48bc7c9aa9a451
Irsyad Jamal Pratama Putra <irsyadjpratamap@gmail.com>
2019-10-05 04:15:57 +0700

    Add Leave Application Response

---
+++ src/main/java/com/lawencon/linov/outsource/payload/response/LeaveApplicationResponse.java
@@ -0,0 +1,104 @@
+package com.lawencon.linov.outsource.payload.response;
+
+import com.lawencon.linov.outsource.model.Image;
+import com.lawencon.linov.outsource.model.authentication.User;
+import com.lawencon.linov.outsource.util.DayType;
+import com.lawencon.linov.outsource.util.LeaveType;
+import com.lawencon.linov.outsource.util.StatusName;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+public class LeaveApplicationResponse {
+
+    private Long id;
+    private Timestamp start;
+    private DayType startDayType;
+    private DayType endDayType;
+    private Timestamp end;
+    private String reason;
+    private ImageSummary document;
+    private UserSummary approver;
+    private StatusName status;
+    private LeaveType leaveType;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Timestamp getStart() {
+        return start;
+    }
+
+    public void setStart(Timestamp start) {
+        this.start = start;
+    }
+
+    public DayType getStartDayType() {
+        return startDayType;
+    }
+
+    public void setStartDayType(DayType startDayType) {
+        this.startDayType = startDayType;
+    }
+
+    public DayType getEndDayType() {
+        return endDayType;
+    }
+
+    public void setEndDayType(DayType endDayType) {
+        this.endDayType = endDayType;
+    }
+
+    public Timestamp getEnd() {
+        return end;
+    }
+
+    public void setEnd(Timestamp end) {
+        this.end = end;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public ImageSummary getDocument() {
+        return document;
+    }
+
+    public void setDocument(ImageSummary document) {
+        this.document = document;
+    }
+
+    public UserSummary getApprover() {
+        return approver;
+    }
+
+    public void setApprover(UserSummary approver) {
+        this.approver = approver;
+    }
+
+    public StatusName getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusName status) {
+        this.status = status;
+    }
+
+    public LeaveType getLeaveType() {
+        return leaveType;
+    }
+
+    public void setLeaveType(LeaveType leaveType) {
+        this.leaveType = leaveType;
+    }
+}
